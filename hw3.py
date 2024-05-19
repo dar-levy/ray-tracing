0d1c174a302ce9a038d60580534da5a3191de3ad
Dar Levy <darlevy8@gmail.com>
2024-05-19 19:03:38 +0300

    Update hw3.py to dev version

--- hw3.py
+++ hw3.py
@@ -3,24 +3,19 @@ import matplotlib.pyplot as plt
 
 def render_scene(camera, ambient, lights, objects, screen_size, max_depth):
     width, height = screen_size
-    ratio = float(width) / height
-    screen = (-1, 1 / ratio, 1, -1 / ratio)  # left, top, right, bottom
+    aspect_ratio = width / height
+    screen = (-1, 1 / aspect_ratio, 1, -1 / aspect_ratio)  # left, top, right, bottom
 
     image = np.zeros((height, width, 3))
 
     for i, y in enumerate(np.linspace(screen[1], screen[3], height)):
         for j, x in enumerate(np.linspace(screen[0], screen[2], width)):
-            # screen is on origin
             pixel = np.array([x, y, 0])
             origin = camera
             direction = normalize(pixel - origin)
             ray = Ray(origin, direction)
-
-            color = np.zeros(3)
             color = calculate_color(camera, ambient, lights, objects, ray, max_depth, 1)
-
-            # We clip the values between 0 and 1 so all pixel values will make sense.
-            image[i, j] = np.clip(color,0,1)
+            image[i, j] = np.clip(color, 0, 1)
 
     return image
 
@@ -30,22 +25,6 @@ def calculate_reflected_color(p, camera, ambient, lights, objects, ray, max_dept
     reflected_ray = Ray(p, normalize(reflected(ray.direction, normal_of_intersection)))
     return kr * calculate_color(camera, ambient, lights, objects, reflected_ray, max_depth, level)
 
-def calculate_refracted_color(p, camera, ambient, lights, objects, ray, max_depth, level, normal_of_intersection, nearest_object):
-    kt = nearest_object.refraction
-    if kt > 0:
-        n1 = 1.0  # Refractive index of air
-        n2 = nearest_object.refractive_index  # Refractive index of the object
-        cos_i = np.dot(ray.direction, -normal_of_intersection)
-        sin_t2 = (n1 / n2) ** 2 * (1 - cos_i ** 2)
-        if sin_t2 > 1:
-            # Total internal reflection
-            return np.zeros(3)
-        cos_t = np.sqrt(1 - sin_t2)
-        refracted_direction = (n1 / n2) * ray.direction + (n1 / n2 * cos_i - cos_t) * normal_of_intersection
-        refracted_ray_origin = p + normal_of_intersection * (nearest_object.radius + 0.001)
-        refracted_ray = Ray(refracted_ray_origin, refracted_direction)
-        return kt * calculate_color(camera, ambient, lights, objects, refracted_ray, max_depth, level)
-    return np.zeros(3)
 
 def calculate_light_contribution(ambient_color, lights, p, normal_of_intersection, camera, objects, nearest_object, min_distance):
     color = np.float64(ambient_color)
@@ -87,7 +66,6 @@ def calculate_color(camera, ambient, lights, objects, ray, max_depth, level):
     ambient_color = calculate_ambient_color(ambient, nearest_object)
     color = calculate_light_contribution(ambient_color, lights, p, normal_of_intersection, camera, objects, nearest_object, min_distance)
     color += calculate_reflected_color(p, camera, ambient, lights, objects, ray, max_depth, level, normal_of_intersection, nearest_object)
-    color += calculate_refracted_color(p, camera, ambient, lights, objects, ray, max_depth, level, normal_of_intersection, nearest_object)
 
     return color
 
@@ -147,6 +125,7 @@ def your_own_scene():
     camera = np.array([0, 1, 1])
 
     return camera, lights, objects
+
 
 def refracted_scene():
     """
